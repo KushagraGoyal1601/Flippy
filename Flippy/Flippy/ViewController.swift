2a838a2abeb791dea33648b820a157638c92bd4a
Kushagra Goyal <kushagragoyal1601@gmail.com>
2025-03-11 16:54:28 +0530

    Base Project

---
+++ Flippy/Flippy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flippy
+//
+//  Created by Kushagra Goyal on 11/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
